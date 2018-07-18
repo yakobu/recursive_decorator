86988928f8743a9cb01f338f57c64840651c0f35
ronen-y <ronenya4321@gmail.com>
2018-07-18 23:58:54 +0300

    travis

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
                        '*.css', '*.xml', '*.rst']},
     classifiers=[
         'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
